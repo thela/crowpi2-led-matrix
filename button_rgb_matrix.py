74c724f10ff28f552c3b406d5819bb12bf23a342
thela <luigi.mazari@gmail.com>
2021-12-08 18:05:01 +0100

    first commit

---
+++ button_rgb_matrix.py
@@ -0,0 +1,39 @@
+
+import time
+import RPi.GPIO as GPIO
+
+from lcd_module import LCDModule
+from led_matrix_module import strip, set_strip_to_random_colour, dim_colours
+from button_matrix_module import ButtonMatrix
+
+
+strip.begin()
+lcd_screen = LCDModule()
+lcd_screen.turn_on()
+buttons = ButtonMatrix()
+
+if __name__ == "__main__":
+
+    try:
+        while True:
+            # get buttons press from SPI
+            adc_key_value = buttons.GetAdcValue()
+            key = buttons.GetKeyNum(adc_key_value)
+            
+            dim_colours(strip)
+            if key != buttons.oldkey:
+                time.sleep(0.05)
+                adc_key_value = buttons.GetAdcValue()
+                key = buttons.GetKeyNum(adc_key_value)
+                if key != buttons.oldkey:
+                    oldkey = key
+                    if key >= 0:
+                        # button pressed, activate it
+                            set_strip_to_random_colour(strip, key)
+                            lcd_screen.write_lcd("pressed -> {0}".format(key))
+            time.sleep(buttons.delay)
+
+    except KeyboardInterrupt:
+        lcd_screen.clear()
+        lcd_screen.turn_off()
+        GPIO.cleanup()
